9f996f610eefcd90b1edad418c1ea8f89febfeed
shafianasir <shafianasir@users.noreply.github.com>
2018-05-05 01:27:46 +0500

    Fixed errors

--- Armageddon/Assets/Scripts/PlayerVitals.cs
+++ Armageddon/Assets/Scripts/PlayerVitals.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerVitals : MonoBehaviour 
+public class PlayerVitals : MonoBehaviour
 {
-	public Slider HealthSlider;
-	public int maxhealth;
-	public int healthfallright;
+    public Slider HealthSlider;
+    public int maxhealth;
+    public int healthfallright;
+    public PlayerStats player;
 
-	void Start(){
-		HealthSlider.maxValue = maxhealth;
-		HealthSlider.value = maxhealth;
-	}
-	void Update(){
-		if (HealthSlider.value <= 0) {
-			HealthSlider.value -= Time.deltaTime / healthfallright* 2;
-	}
+    void Start()
+    {
+        HealthSlider.maxValue = maxhealth;
+        HealthSlider.value = maxhealth;
+    }
+    void Update()
+    {
+        HealthSlider.value = player.Health;
+        if (HealthSlider.value <= 0)
+        {
+            HealthSlider.value -= Time.deltaTime / healthfallright * 2;
+        }
+    }
 }
