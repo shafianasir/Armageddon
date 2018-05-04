5591b40743b2d60112b94a6f2f7a2e61856df5e4
ally0106 <38324061+ally0106@users.noreply.github.com>
2018-05-04 10:55:44 -0700

    kk
    ll

---
+++ Armageddon/Assets/Scripts/PlayerVitals.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerVitals : MonoBehaviour 
+{
+	public Slider HealthSlider;
+	public int maxhealth;
+	public int healthfallright;
+
+	void Start(){
+		HealthSlider.maxValue = maxhealth;
+		HealthSlider.value = maxhealth;
+	}
+	void Update(){
+		if (HealthSlider.value <= 0) {
+			HealthSlider.value -= Time.deltaTime / healthfallright* 2;
+	}
+}
