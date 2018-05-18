75dc7876cfbba1010be8aa1e6eeacd60e48a3898
ally0106 <38324061+ally0106@users.noreply.github.com>
2018-05-18 10:34:53 -0700

    bbb
    bbb

---
+++ Armageddon/Assets/connectingjoystick.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class connectingjoystick : MonoBehaviour {
+
+	protected Joystick joystick;
+	protected FixedButton joybutton;
+
+	protected bool jump;
+
+	// Use this for initialization
+	void Start () {
+		joystick = FindObjectOfType<Joystick>();
+		joybutton = FindObjectOfType<FixedButton>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var rigidbody = GetComponent<Rigidbody>();
+		rigidbody.velocity = new Vector3(joystick.Horizontal* 100f, rigidbody.velocity.y,joystick.Vertical* 100f );
+
+		if (!jump && joybutton.Pressed) {
+			jump = true;
+			rigidbody.velocity += Vector3.up * 100f;
+		}
+
+		if (jump && !joybutton.Pressed) {
+
+			jump = false;
+		}
+	}
+}
