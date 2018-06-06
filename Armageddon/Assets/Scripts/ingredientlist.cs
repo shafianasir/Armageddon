014db7e22511dd0fa9ab49639bbea6f9ba5d12de
ally0106 <38324061+ally0106@users.noreply.github.com>
2018-06-06 12:11:55 -0700

    ff
    sal.

---
+++ Armageddon/Assets/Scripts/ingredientlist.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ingredientlist : MonoBehaviour {
+
+	public GameObject IngredientText;
+
+
+	void OnTriggerEnter(Collider box){
+		if (box.CompareTag ("Player")) {
+			IngredientText.SetActive(true);
+			GameObject IngredintUI = GameObject.Find("IngredientList");
+			IngredintUI.GetComponent<Button>().interactable = true;
+		}
+	}
+
+}
