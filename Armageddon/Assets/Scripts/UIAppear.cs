e0d9b2c09244d02a2096c8af3a39d58ee7f4aefd
shafianasir <shafianasir@users.noreply.github.com>
2018-06-07 16:25:43 +0500

    enable ingredients

--- Armageddon/Assets/Scripts/UIAppear.cs
+++ Armageddon/Assets/Scripts/UIAppear.cs
@@ -6,15 +6,20 @@ using UnityEngine.UI;
 public class UIAppear : MonoBehaviour {
 
 	[SerializeField] private Image customimage;
+    public GameObject ingredients;
 
-	void OnTriggerEnter(Collider box){
+    void OnTriggerEnter(Collider box){
 		if (box.CompareTag ("Player")) {
 			customimage.enabled = true;
 		}
-	}
+        ingredients.SetActive(true);
+    }
+
 	void OnTriggerExit(Collider box){
 		if (box.CompareTag ("Player")) {
 			customimage.enabled = false;
 		}
 	}
+
+
 }
