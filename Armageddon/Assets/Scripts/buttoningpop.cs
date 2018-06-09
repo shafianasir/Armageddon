cb1a6efe606cb8a4ae9f21490f0271bf0007d40b
ally0106 <38324061+ally0106@users.noreply.github.com>
2018-06-09 15:11:09 -0700

    ss
    sss

---
+++ Armageddon/Assets/Scripts/buttoningpop.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttoningpop : MonoBehaviour {
+	[SerializeField] private Image customimage;
+
+	
+
+	public void OnClickButton() {
+		
+		customimage.enabled = true;
+	}
+}
